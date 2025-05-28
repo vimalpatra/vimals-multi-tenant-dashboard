e24e53c94ec3549e8e37fb0fffc73e209126bb82
vimalpatra <vimalpatra1@gmail.com>
2025-05-28 05:42:17 +0530

    filename issue fixed

--- shared/ui/skeleton.jsx
+++ shared/ui/skeleton.jsx
@@ -1,6 +1,6 @@
 export function Skeleton({ className = "" }) {
   return (
-    <div className={`animate-pulse ${className}`}>
+    <div className={`animate-pulse  ${className}`}>
       <div className="h-6 bg-gray-200 rounded w-1/3 mb-4"></div>
       <div className="h-4 bg-gray-200 rounded w-2/3"></div>
     </div>
